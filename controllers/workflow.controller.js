fbb91da0d596bf7d16abfef3d45823e4053e1cad
Anurag-xo <amnrag889@gmail.com>
2025-10-25 16:48:13 +0000

    feat(workflows): add workflow routing, move Arcjet middleware before routes
    - Import workflowRouter for workflow-related endpoints
    - Register /api/v1/workflows route for workflow operations]
    - Supports scheduled tasks and background job management

---
+++ controllers/workflow.controller.js
@@ -0,0 +1,54 @@
+import { createRequire } from "module";
+import dayjs from "dayjs";
+const require = createRequire(import.meta.url);
+const { serve } = require("@upstash/workflow/express");
+import subscription from "../models/subscription.model.js";
+
+const REMINDERS = [7, 5, 2, 1];
+
+export const sendReminders = serve(async () => {
+  const { subscriptionId } = context.requestPayload;
+  const subscription = await fetchSubscription(context, subscriptionId);
+
+  if (!fetchSubscription || subscription.status != "active") return;
+
+  const renewalDate = dayjs(subscription.renewalDate);
+
+  if (renewalDate.isBefore(dayjs())) {
+    console.log(
+      `Rnewal date has passed for subscription ${subscriptionId}. Stopping workflow.`,
+    );
+    return;
+  }
+
+  for (const daysBefore of REMINDERS) {
+    const reminderDate = renewalDate.subtract(daysBefore, "day");
+
+    if (reminderDate.isAfter(dayjs())) {
+      await sleepUntilReminder(
+        context,
+        `Reminder ${daysBefore} days before`,
+        reminderDate,
+      );
+    }
+    await triggerReminder(context, `Reminder ${daysBefore} days before`);
+  }
+});
+
+const fetchSubscription = async (context, subscriptionId) => {
+  return await context.run("get subscription", async () => {
+    return subscription.findById(subscriptionId).populate("user", "name email");
+  });
+};
+
+const sleepUntilReminder = async (context, label, date) => {
+  console.log(`Sleeping until ${label} reminder at ${date}`);
+  await context.sleepUntil(label, date.toDate());
+};
+
+const triggerReminder = async (context, label) => {
+  return await context.run(label, () => {
+    console.log(`Triggering ${label} reminder`);
+    // Send email
+  });
+};
