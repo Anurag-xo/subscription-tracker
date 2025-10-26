45da88c0dbd5bcf62983492db7e78d6a618b3cc2
Anurag-xo <amnrag889@gmail.com>
2025-10-26 09:37:28 +0000

    feat(helmet,healthcheck): add helmet and health check global handlers

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import process from "process";
 
 import { PORT } from "./config/env.js";
-
-import helmet from "helmet";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
@@ -14,11 +14,11 @@ import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(arcjetMiddlware);
-app.use(helmet());
 
 //middlewares
 app.use("/api/v1/auth", authRouter);
@@ -26,10 +26,28 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/workflows", workflowRouter);
 
-app.use(errorMiddleware);
+// HEALTH CHECK
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", uptime: process.uptime() });
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to the subscription Tracker API:>");
+});
+
+app.use(errorMiddleware);
+
+// GLOBAL ERROR HANDLERS
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION! Shutting down...");
+  console.errorr(err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION! Shutting down...");
+  console.error(err);
+  process.exit(1);
 });
 
 app.listen(PORT, async () => {
