49463c5ef4e5e0547dd6a03e39c75c6ddc052400
Anurag-xo <amnrag889@gmail.com>
2025-10-30 20:19:47 +0000

    fix(auth.routes.js): add express-valiator for input validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,25 @@
 import { Router } from "express";
+import { body } from "express-validator";
 
 import { signUp, signIn, signOut } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-// Path: /api/v1/auth/sign-up (POST)
-authRouter.post("/sign-up", signUp);
+authRouter.post(
+  "/sign-up",
+  [
+    body("name").trim().isLength({ min: 2, max: 50 }),
+    body("email").isEmail().normalizeEmail(),
+    body("password").isLength({ min: 6 }),
+  ],
+  signUp,
+);
 
-// Path: /api/v1/auth/sign-in (POST)
-authRouter.post("/sign-in", signIn);
+authRouter.post(
+  "/sign-in",
+  [body("email").isEmail(), body("password").exists()],
+  signIn,
+);
 
 // Path: /api/v1/auth/sign-out (POST)
 authRouter.post("/sign-out", signOut);
